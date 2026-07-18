4b4473b5a6e3716a3988584c16567c779f6b8696
sumanjhanp04 <181665035+codecsuman@users.noreply.github.com>
2026-07-19 05:15:06 +0530

    Fix Render deployment issues

--- server/dist/utils/generateToken.d.ts
+++ server/dist/utils/generateToken.d.ts
@@ -1,5 +1,5 @@
-import { IUserDocument } from "../models/user.model.js";
 import { Response } from "express";
+import { IUserDocument } from "../models/user.model.js";
 export declare const generateToken: (res: Response, user: IUserDocument) => string;
 export declare const clearToken: (res: Response) => void;
 //# sourceMappingURL=generateToken.d.ts.map
--- server/dist/utils/generateToken.js
+++ server/dist/utils/generateToken.js
@@ -13,19 +13,21 @@ export const generateToken = (res, user) => {
     const isProduction = process.env.NODE_ENV === "production";
     res.cookie("token", token, {
         httpOnly: true,
-        sameSite: isProduction ? "none" : "lax",
         secure: isProduction,
+        sameSite: isProduction ? "none" : "lax",
+        path: "/",
         maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     return token;
 };
-// Helper to clear token (for logout)
+// Clear token (Logout)
 export const clearToken = (res) => {
     const isProduction = process.env.NODE_ENV === "production";
     res.cookie("token", "", {
         httpOnly: true,
-        sameSite: isProduction ? "none" : "lax",
         secure: isProduction,
+        sameSite: isProduction ? "none" : "lax",
+        path: "/",
         expires: new Date(0),
     });
 };
